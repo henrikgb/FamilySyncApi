2ce9529a34b7e3209372c623d5cc3b010f4e8e2c
Henrik Grimsmo Busengdal <henrik-gb@gotmail.com>
2025-04-16 21:27:26 +0200

    Feat: update shopping list item

--- Models/ShoppingItem.cs
+++ Models/ShoppingItem.cs
@@ -5,4 +5,5 @@ public class ShoppingItem
     public string Name { get; set; } = string.Empty;
     public int Quantity { get; set; }
     public bool IsCompleted { get; set; }
+    public string? Category { get; set; }
 }
