aad8a71a2dbcf17fd81dfac6bb3efc37f24b38cd
Henrik Grimsmo Busengdal <henrik-gb@gotmail.com>
2025-04-23 23:51:50 +0200

    Feat: Update data model

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,6 +3,6 @@ namespace FamilySyncApi.Models;
 public class TodoItem
 {
     public string Task { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
     public bool IsCompleted { get; set; }
 }
