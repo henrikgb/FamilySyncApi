35b3004ace981b09c5cece414e5bc653e8d97c73
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-05-27 11:08:34 +0200

    Feat: check for user roles in controllers

--- Controllers/TodoListController.cs
+++ Controllers/TodoListController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace FamilySyncApi.Controllers;
 
-[Authorize]
+[Authorize(Roles = "FamilySyncUser")]
 [ApiController]
 [Route("api/[controller]")]
 public class TodoListController : ControllerBase
